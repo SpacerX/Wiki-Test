1bae47b2a187af569445da1f24733688bbac2277
SpacerX <rbe@spacerx.de>
2023-09-05 21:48:42 +0200

    Add files via upload
    Signed-off-by: SpacerX <rbe@spacerx.de>

---
+++ sniffing_einrichten/start_mitmweb.bat
@@ -0,0 +1,3 @@
+%userprofile%\.local\bin\mitmweb.exe --listen-port 9090 --web-port 9091 --mode wireguard -s C:\ioBroker\node_modules\iobroker.roborock\lib\sniffing\mitmproxy_roborock.py
+Pause
+Exit
